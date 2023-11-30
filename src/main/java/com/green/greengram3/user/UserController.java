db6cc9dc190e8e2a7b3e4c0ce68f8957f6eba1fd
DoheumPark <pirbak@hanmail.net>
2023-11-30 09:59:09 +0900

    23-11-30 1교시 (회원가입)

--- src/main/java/com/green/greengram3/user/UserController.java
+++ src/main/java/com/green/greengram3/user/UserController.java
@@ -1,12 +1,30 @@
 package com.green.greengram3.user;
 
+import com.green.greengram3.common.ResVo;
+import com.green.greengram3.user.model.UserSignupDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
     private final UserService service;
+
+    @PostMapping("/signup")
+    @Operation(summary = "회원가입", description = "회원가입 처리")
+    public ResVo postSignup(@RequestBody UserSignupDto dto) {
+        log.info("dto: {}", dto);
+        return service.signup(dto);
+    }
 }
--- src/main/java/com/green/greengram3/user/UserService.java
+++ src/main/java/com/green/greengram3/user/UserService.java
@@ -1,10 +1,34 @@
 package com.green.greengram3.user;
 
+import com.green.greengram3.common.ResVo;
+import com.green.greengram3.user.model.UserSignupDto;
+import com.green.greengram3.user.model.UserSignupProcDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserMapper mapper;
+
+    public ResVo signup(UserSignupDto dto) {
+        String salt = BCrypt.gensalt();
+        String hashedPw = BCrypt.hashpw(dto.getUpw(), salt);
+        //비밀번호 암호화
+
+        UserSignupProcDto pDto = new UserSignupProcDto();
+        pDto.setUid(dto.getUid());
+        pDto.setUpw(hashedPw);
+        pDto.setNm(dto.getNm());
+        pDto.setPic(dto.getPic());
+
+        log.info("before - pDto.iuser : {}", pDto.getIuser());
+        int affectedRows = mapper.insUser(pDto);
+        log.info("after - pDto.iuser : {}", pDto.getIuser());
+
+        return new ResVo(pDto.getIuser()); //회원가입한 iuser pk값이 리턴
+    }
 }
